a78b4ef4e92824bd1cb49c18bdaab51d0d5baf23
Randy Fournier <ranfouprodev@gmail.com>
2020-01-24 11:33:01 -0700

    initial commit installation of vue with basic mock table

---
+++ planetsnu/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+   <CodingChallenge msg="initial commit" />
+  </div>
+</template>
+
+<script>
+import CodingChallenge from './components/CodingChallenge.vue'
+
+export default {
+  name: 'app',
+  components: {
+    CodingChallenge
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ planetsnu/src/components/CodingChallenge.vue
@@ -0,0 +1,56 @@
+<template>
+  
+  <table>
+    <thead>
+        <tr>
+            <th>Name</th>
+        
+            <th>Short Description</th>
+        
+            <th>Date Created</th>
+        
+            <th>Created By</th>
+        
+            <th>Host Days</th>
+       
+            <th>Password</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>The table body</td>
+            <td>with two columns</td>
+            <td>The table body</td>
+            <td>with two columns</td>
+            <td>The table body</td>
+        </tr>
+    </tbody>
+</table>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
