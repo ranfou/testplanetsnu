187f231368621bc050aa9bd132597c278609fa51
Randy Fournier <ranfouprodev@gmail.com>
2020-01-24 12:40:22 -0700

    list of games into table

--- planetsnu/src/components/CodingChallenge.vue
+++ planetsnu/src/components/CodingChallenge.vue
@@ -17,17 +17,16 @@
         </tr>
     </thead>
     <tbody>
-        <tr>
-            <td>The table body</td>
-            <td>with two columns</td>
-            <td>The table body</td>
-            <td>with two columns</td>
-            <td>The table body</td>
+        <tr v-for="game in games" :key="game.name">
+            <td>{{ game.name }}</td>
+            <td>{{ game.shortdescription }}</td>
+            <td>{{ game.datecreated }}</td>
+            <td>{{ game.createdby }}</td>
+            <td>{{ game.hostdays }}</td>
+            <td>{{ game.password }}</td>
         </tr>
     </tbody>
 </table>
-
-    {{ games }}  
 
 <button v-on:click="fetchUsers">FetchGames</button>
 </div>
@@ -47,7 +46,7 @@ export default {
   },
   methods: {
     fetchUsers: function () {
-      const baseURI = 'http://api.planets.nu/games/list'
+      const baseURI = 'http://api.planets.nu/games/list?status=1'
       this.$http.get(baseURI)
       .then((result) => {
         this.games = result.data
